2c8b40f9b4e1e42e191e058a95a75f747ada3d65
LysenkoWork <lyss.work@gmail.com>
2023-04-08 23:29:37 +0300

    html2

--- lib/model/models.dart
+++ lib/model/models.dart
@@ -26,16 +26,53 @@ class Partner {
   Partner(this.id, this.name, this.photo);
 }
 
+class Expert {
+  String? id;
+  String? name;
+  String? job;
+  String? photo;
+  Expert(this.id, this.name, this.job, this.photo);
+}
+
 var partners = [
-  Partner('1','',''),
-  Partner('','',''),
-  Partner('','','')
+  Partner('1','ООО "ARТЕЛЬ DIGITAL"','assets/partners/IMG-20230323-WA0017.jpg'),
+  Partner('2','ООО "Экстраверт"','assets/partners/IMG-20230331-WA0032.jpg'),
+  Partner('3','Союз Дизайнеров России','assets/partners/IMG-20230331-WA0033.jpg'),
+  Partner('4','Технопарк КубГУ "Точка Кипения" КубГУ','assets/partners/IMG-20221010-WA0099.jpg'),
+  Partner('5','ООО "Рекламный дом Кубани"','assets/partners/IMG-20230402-WA0036.jpg'),
+  Partner('6','газета "Кубанские новости"','assets/partners/IMG-20230401-WA0006.jpg'),
+  Partner('7','телеграм-канал "Наука и точка"','assets/partners/IMG-20230401-WA0005.jpg')
 ];
 
-var organization = [
-  Partner('1','','IMG-20230331-WA0016.jpg'),
-  Partner('','',''),
-  Partner('','','')
+var organizers = [
+  Partner('1','Кафедра ДПИ и дизайна художественно-графического факультета','assets/organizers/IMG-20230331-WA0018.jpg'),
+  Partner('2','Кафедра анализа данных и искусственного интеллекта факультета компьютерных технологий и прикладной математики','assets/organizers/IMG-20230331-WA0017.jpg'),
+  Partner('3','Кафедра истории и правового регулирования массовых коммуникаций факультета журналистики','assets/organizers/IMG-20230331-WA0016.jpg'),
+  Partner('4','МУ ДО "Детская художественная школа" им. В.А. Филиппова МО г. Краснодар','assets/organizers/IMG-20230331-WA0019.jpg'),
+  Partner('5','Физико-технический факультет КубГУ','assets/organizers/IMG-20230331-WA0021.jpg')
+];
+
+var experts = [
+  Expert('1','Шарафан М.В.', 'Проректор по научной работе и инновациям КубГУ','assets/experts/sharafan.jpg'),
+  Expert('1','Коробко Ю.В.', 'Декан художественно-графического факультета КубГУ','assets/experts/Korobko.jpg'),
+  Expert('1','Морозкина Е.А.', 'Председатель орг комитета выставки-конкурса, зав. кафедры ДПИ и дизайна ХГФ КубГУ','assets/experts/morozkina.jpg'),
+  Expert('1','Денисенко В.И.', 'Профессор кафедры живописи, зам. декана по науке ХГФ КубГУ','assets/experts/denisenko.jpg'),
+  Expert('1','Копонева О.В.', 'Директор Технопарка КубГУ','assets/experts/koponeva.jpg'),
+  Expert('1','Волкова Т.А.', 'Программный директор «Точка кипения КубГУ»','assets/experts/volkova.jpg'),
+  Expert('1','Коваленко А.В.', 'Зав. кафедры анализа данных и искусственного интеллекта факультета компьютерных технологий и прикладной математики КубГУ','assets/experts/kovalenko.jpg'),
+  Expert('1','Лучинский Ю.В.', 'Зав. кафедры истории и правового регулирования массовых коммуникаций КубГУ','assets/experts/lychinskiy.jpg'),
+  Expert('1','Зимина О.А.', 'Председатель Краснодарского регионального отделения общественной организации «Союз дизайнеров России», и.о. декана факультета архитектуры и дизайна, зав. кафедры «Дизайн одежды» ФАД КубГУ','assets/experts/zimina.jpg'),
+  Expert('1','Говорова Г.Н.', 'Директор Департамента по международным связям','assets/experts/govorova.jpg'),
+  Expert('1','Ротай Е.М.', 'Зам. директора Департамента по международным связям','assets/experts/rotai.jpg'),
+  Expert('1','Белая Т.В.', 'Доцент кафедры ДПИ и дизайна ХГФ КубГУ, зам. декана по учебной работе ХГФ','assets/experts/belpay.jpg'),
+  Expert('1','Прокопова А.С', 'Ст. преподаватель кафедры ДПИ и дизайна ХГФ КубГУ','assets/experts/prokopova.jpg'),
+  Expert('1','Полонникова А.А.', 'Преподаватель кафедры ДПИ и дизайна ХГФ КубГУ','assets/experts/polonnikova.jpg'),
+  Expert('1','Присяжнюк В.С.', 'Директор детской художественной школы им В.А.Филиппова','assets/experts/prosygnik.jpg'),
+  Expert('1','Шведчиков Н.Ю.', 'Генеральный директор ООО Экстраверт','assets/experts/svedchikov.jpg'),
+  Expert('1','Золотарёв С.С.', 'Генеральный директор ООО «М-АРТ»','assets/experts/zolotarev.jpg'),
+  Expert('1','Зыкова Е.В.', 'Управляющий партнёр «ARTEЛЬ DIGITAL»','assets/experts/zikova.jpg'),
+  Expert('1','Соколова', 'Настоящий знаток и ценитель','assets/experts/sokolova.jpg'),
+  Expert('1','Фоменко Михаил', 'Настоящий знаток и ценитель','assets/experts/fomenko.jpg')
 ];
 
 class User {
